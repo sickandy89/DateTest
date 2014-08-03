3471cb5be3fa8dbc62e4935baa92050c0979c11b
Andrej <andy_da_god@web.de>
2014-08-03 19:31:49 +0200

    Initial checkin

---
+++ Datetest/src/mymain.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+
+
+public class mymain {
+public static void main(String[] args) {
+		
+	Calendar c = Calendar.getInstance();
+	System.out.println(c.get(Calendar.HOUR_OF_DAY));
+	System.out.println(c.get(Calendar.MINUTE));
+	System.out.println(c.get(Calendar.MONTH));
+}
+}
