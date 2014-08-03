ed643a098f0360a12b4fd5a08dd2e5c6d7a0d1bb
Andrej <andy_da_god@web.de>
2014-08-03 19:42:10 +0200

    add output change

--- Datetest/src/mymain.java
+++ Datetest/src/mymain.java
@@ -8,5 +8,6 @@ public static void main(String[] args) {
 	System.out.println(c.get(Calendar.HOUR_OF_DAY));
 	System.out.println(c.get(Calendar.MINUTE));
 	System.out.println(c.get(Calendar.MONTH));
+	System.out.println("Test");
 }
 }
